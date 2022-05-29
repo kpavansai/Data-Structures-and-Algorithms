175e0a5d4d67d75b039e20f8cd9a8eb24105d233
kpavansai <kpavan.sai1@gmail.com>
2022-05-29 22:45:34 +0530

    Time: 38 ms (75.00%), Space: 13.7 MB (100.00%) - LeetHub

--- 2283-check-if-number-has-equal-digit-count-and-digit-value/2283-check-if-number-has-equal-digit-count-and-digit-value.py
+++ 2283-check-if-number-has-equal-digit-count-and-digit-value/2283-check-if-number-has-equal-digit-count-and-digit-value.py
@@ -1,12 +1,14 @@
 class Solution:
     def digitCount(self, num: str) -> bool:
-        result = [0] * 10
         
-        for i in num:
-            result[int(i)] += 1
-            
+        map1 = {}
+        
+        map2 = {}
+        
         for i in range(len(num)):
-            if int(num[i]) != result[i]:
-                return False
-        return True
-        
+            
+            map1[i] = num[i]
+            
+            map2[i] = str(num.count(str(i)))
+            
+        return map1 == map2
